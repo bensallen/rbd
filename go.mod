08a55de2a7f6627ff3e8be1a9542ace47f3de26d
J. Lowell Wofford <lowell@lanl.gov>
2020-11-19 11:36:27 -0700

    update dependences, force u-root to newest master commit with replace rule

--- go.mod
+++ go.mod
@@ -3,7 +3,13 @@ module github.com/bensallen/rbd
 go 1.14
 
 require (
+	github.com/google/goexpect v0.0.0-20200816234442-b5b77125c2c5 // indirect
+	github.com/rekby/gpt v0.0.0-20200614112001-7da10aec5566 // indirect
 	github.com/spf13/pflag v1.0.5
-	github.com/u-root/u-root v6.0.0+incompatible
-	golang.org/x/sys v0.0.0-20200413165638-669c56c373c4
+	github.com/stretchr/testify v1.6.1 // indirect
+	github.com/u-root/u-root v7.0.0+incompatible
+	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68
+	golang.org/x/tools v0.0.0-20201119174615-0557df368a99 // indirect
 )
+
+replace github.com/u-root/u-root v7.0.0+incompatible => github.com/u-root/u-root v1.0.1-0.20201119150355-04f343dd1922
--- vendor/github.com/u-root/u-root/pkg/mount/loop/loop_linux.go
+++ vendor/github.com/u-root/u-root/pkg/mount/loop/loop_linux.go
@@ -1,4 +1,4 @@
-// Copyright 2018 the u-root Authors. All rights reserved
+// Copyright 2018-2020 the u-root Authors. All rights reserved
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -9,7 +9,6 @@ package loop
 
 import (
 	"github.com/u-root/u-root/pkg/mount"
-	"golang.org/x/sys/unix"
 )
 
 // Loop represents a regular file exposed as a loop block device.
@@ -27,19 +26,15 @@ type Loop struct {
 
 	// Data is the data to pass to mount(2).
 	Data string
-
-	// Mounted indicates whether the device has been mounted.
-	Mounted bool
-
-	// dir is the directory the block device was mounted on.
-	dir string
 }
+
+var _ mount.Mounter = &Loop{}
 
 // New initializes a Loop struct and allocates a loop device to it.
 //
 // source is the file to use as a loop block device. fstype the file system
 // name. data is the data argument to the mount(2) syscall.
-func New(source, fstype string, data string) (mount.Mounter, error) {
+func New(source, fstype string, data string) (*Loop, error) {
 	devicename, err := FindDevice()
 	if err != nil {
 		return nil, err
@@ -55,27 +50,20 @@ func New(source, fstype string, data string) (mount.Mounter, error) {
 	}, nil
 }
 
-// Mount mounts the provided source file, with type fstype, and flags and data options
-// (which are usually 0 and ""), using the allocated loop device.
-func (l *Loop) Mount(path string, flags uintptr) error {
-	l.dir = path
-	if err := unix.Mount(l.Dev, path, l.FSType, flags, l.Data); err != nil {
-		return err
-	}
-	l.Mounted = true
-	return nil
+// DevName implements mount.Mounter.
+func (l *Loop) DevName() string {
+	return l.Dev
 }
 
-const forceUnmount = unix.MNT_FORCE | unix.MNT_DETACH
+// Mount mounts the provided source file, with type fstype, and flags and data options
+// (which are usually 0 and ""), using the allocated loop device.
+func (l *Loop) Mount(path string, flags uintptr) (*mount.MountPoint, error) {
+	return mount.Mount(l.Dev, path, l.FSType, l.Data, flags)
+}
 
-// Unmount unmounts and frees a loop. If it is mounted, it will try to unmount it.
-// If the unmount fails, we try to free it anyway, after trying a more
-// forceful unmount.
-func (l *Loop) Unmount(flags int) error {
-	if l.Mounted {
-		if err := unix.Unmount(l.dir, flags); err != nil {
-			unix.Unmount(l.dir, flags|forceUnmount)
-		}
-	}
+// Free frees the loop device.
+//
+// All mount points must have been unmounted prior to calling this.
+func (l *Loop) Free() error {
 	return ClearFile(l.Dev)
 }
