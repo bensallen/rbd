fe3c9c795d7efd62b7a4d1b2e0231eaa404cb60f
J. Lowell Wofford <lowell@lanl.gov>
2020-11-23 11:21:25 -0700

    add pid_init functionality

--- pkg/boot/boot.go
+++ pkg/boot/boot.go
@@ -1,6 +1,9 @@
 package boot
 
-import "log"
+import (
+	"log"
+	"os"
+)
 
 // UnshareRoot spawns the image in newRoot as a new container.
 // This is a blocking execution.
@@ -15,6 +18,18 @@ func UnshareRoot(newRoot, init string) error {
 // SwitchRoot is a wrapper for OS specific implementation(s)
 func SwitchRoot(newRoot, init string) error {
 	return switchRoot(newRoot, init)
+}
+
+// PIDInit initializes a PID namespace and execs the provided init
+// It can be called at the begining of any main() function and will only do anythin
+// if the environment indicates that it should.
+func PIDInit() {
+	init := os.Getenv("RBD_INIT")
+	if init == "" {
+		return
+	}
+	pidInit(init)
+	os.Exit(0) // need to stop execution should this return
 }
 
 // logging should probably have a global context, but this will simplify things for now
